778525bf7c14b6793dd9af5d729313ac194fa3a8
Said <said.ozgat@hotmail.com>
2021-08-09 14:04:33 +0300

    isAdmin Middleware

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/panel', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['middleware' => ['auth', 'isAdmin'],'prefix'=>'admin'],function (){
+
+});
