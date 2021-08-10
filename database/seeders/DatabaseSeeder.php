5095493aeb1d2c73ec15c377208c26626f5e34e5
Said <said.ozgat@hotmail.com>
2021-08-10 17:23:44 +0300

    Quiz Migration, Factory Seed, Each Seed call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::insert([
-            'name'=>'Said Özgat',
-            'email'=>'said.ozgat@hotmail.com',
-            'email_verified_at' => now(),
-            'type'=>'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,
         ]);
-
-        \App\Models\User::factory(5)->create();
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+            'name'=>'Said Özgat',
+            'email'=>'said.ozgat@hotmail.com',
+            'email_verified_at' => now(),
+            'type'=>'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\User::factory(5)->create();
+    }
+}
