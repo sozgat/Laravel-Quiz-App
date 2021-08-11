b76457fe1bc1a425df9661ed26e82a8422254644
Said <said.ozgat@hotmail.com>
2021-08-12 01:34:29 +0300

    Question migration, factory, seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             QuizSeeder::class,
+            QuestionSeeder::class,
         ]);
     }
 }
